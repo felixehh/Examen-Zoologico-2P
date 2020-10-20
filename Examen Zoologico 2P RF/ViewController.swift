7a212a3691f3e04a11bfe4484f450b23f79c2290
Helliud <javiereliudliz@gmail.com>
2020-10-19 21:23:26 -0700

    Initial Commit

---
+++ Examen Zoologico 2P RF/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Examen Zoologico 2P RF
+//
+//  Created by Alumno on 10/19/20.
+//  Copyright © 2020 ulsa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Examen Zoologico 2P RFTests/Examen_Zoologico_2P_RFTests.swift
@@ -0,0 +1,34 @@
+//
+//  Examen_Zoologico_2P_RFTests.swift
+//  Examen Zoologico 2P RFTests
+//
+//  Created by Alumno on 10/19/20.
+//  Copyright © 2020 ulsa. All rights reserved.
+//
+
+import XCTest
+@testable import Examen_Zoologico_2P_RF
+
+class Examen_Zoologico_2P_RFTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
